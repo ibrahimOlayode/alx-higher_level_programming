b0b9e0cfe9a85a5dd38bc243bd0374a3cdb12282
olibrahim <olayodeibrahim123@gmail.com>
2022-11-30 07:38:20 -0800

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - olayode ibrahim
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - olayode ibrahim
+
+def pow(a, b):
+    return (a ** b)
